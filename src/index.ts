0f5fd562fa0f42651b3754b23ccbd1772f50b96a
surafel habte <surafelhabte1@gmail.com>
2024-05-23 23:20:05 +0300

    index file fr the projects

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from "express";
+import { json } from "body-parser";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+import { crudRoute } from "./controllers/crud";
+import { utilRoute } from "./controllers/util";
+import cors from 'cors';
+
+export const routes = express.Router();
+dotenv.config();
+
+const app = express();
+app.use(json());
+
+// Use CORS middleware
+app.use(cors());
+
+routes.use(crudRoute);
+routes.use(utilRoute);
+
+const prisma = new PrismaClient();
+async function main() {}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+app.use("/", routes);
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
