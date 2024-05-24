61ea7f84b0f786155693d8ce4e28fe2438e360ab
surafel habte <surafelhabte1@gmail.com>
2024-05-24 13:45:39 +0300

    miner

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,3 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+import app from "../src/index";
 
-app.get("/", (req: any, res: any) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-module.exports = app;
+export default app;
