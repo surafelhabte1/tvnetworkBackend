f89dcc6128b6d3d0abd921d4933f07d2ef326ee4
surafel habte <surafelhabte1@gmail.com>
2024-05-24 15:23:52 +0300

    miner

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -186,7 +186,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
