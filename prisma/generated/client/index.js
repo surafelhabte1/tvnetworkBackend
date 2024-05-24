f5f7899a370e6cc718105ea200caab07a30bede3
surafel habte <surafelhabte1@gmail.com>
2024-05-24 15:19:08 +0300

    miner

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -187,7 +187,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
+// import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 import { json } from "body-parser";
 import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
 
 import { crudRoute } from "./controllers/crud";
 import { utilRoute } from "./controllers/util";
@@ -18,17 +18,17 @@ app.use(cors());
 routes.use(crudRoute);
 routes.use(utilRoute);
 
-const prisma = new PrismaClient();
-async function main() {}
+// const prisma = new PrismaClient();
+// async function main() {}
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
 
 app.use("/", routes);
 
